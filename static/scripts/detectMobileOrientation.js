1d78528924e21b4e3c038fe3e32475825024b374
afzidkarim <fzkarim@goodcomy.com>
2024-10-11 00:30:53 +0100

    add script and file detcMobil

---
+++ static/scripts/detectMobileOrientation.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var mobileGif = document.getElementById("mobileGif");
+  
+    function toggleGif(display) {
+        mobileGif.style.display = display ? "block" : "none";
+    }
+  
+    function isMobile() {
+        return /iPhone|iPad|Android/i.test(navigator.userAgent);
+    }
+  
+    function isPortrait() {
+        return window.orientation === 0 || window.orientation === 180;
+    }
+  
+    function handleScreenChange() {
+        if (isMobile() && window.innerWidth <= 500 && isPortrait()) {
+            toggleGif(true);
+        } else {
+            toggleGif(false);
+        }
+    }
+  
+    window.addEventListener("orientationchange", handleScreenChange);
+    window.addEventListener("resize", handleScreenChange);
+  
+    handleScreenChange(); // Initial check
+  });
+  
